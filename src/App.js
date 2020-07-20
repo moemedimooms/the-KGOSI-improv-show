3e88fa194923fc607bbf25b838017fd086bb9fb9
moemedi.mooms <moemedi.mooms@gmail.com>
2020-07-20 05:39:57 +0200

    added DynamicContentWrapper container component & the data it requires

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Header from "./components/presentational/Header";
+import DynamicContentWrapper from "./components/container/DynamicContentWrapper";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <DynamicContentWrapper />
     </div>
   );
 }
