2e43277d2ce90d4be003bbaa9d7e4807b63d52bc
moemedi.mooms <moemedi.mooms@gmail.com>
2020-07-21 05:15:53 +0200

    implemented basic animation to JSX partial of Scenarios component

--- src/components/presentational/Scenarios/index.js
+++ src/components/presentational/Scenarios/index.js
@@ -1,24 +1,46 @@
 import React from "react";
+import { useTrail, animated } from "react-spring";
 
 const Scenarios = (props) => {
+  const springs = useTrail(props.scenarios.length, {
+    from: {
+      opacity: 0.2,
+      transform: "translateY(-5px)",
+    },
+    to: {
+      opacity: 1,
+      transform: "translateY(0px)",
+    },
+    config: {
+      mass: 7,
+    },
+  });
+
   return (
     <div className="card bg-light">
       <div className="card-body text-center">
         <h3 class="card-subtitle mb-2 text-muted">available scenarios</h3>
-        {props.scenarios.map((s) => {
+        {springs.map((spring, index) => {
           return (
-            <a className="card-link text-nowrap text-center" key={s.id}>
-              <button
-                type="button"
-                class="btn-lg btn-light mt-3"
-                onClick={props.handleShowReaction}
+            <a
+              className="card-link text-nowrap text-center"
+              key={props.scenarios[index].id}
+            >
+              <animated.span
+                key={index}
+                style={{ ...spring, marginRight: "10px" }}
               >
-                {s.name}
-              </button>
+                <button
+                  type="button"
+                  class="btn-lg btn-light mt-3"
+                  onClick={props.handleShowReaction}
+                >
+                  {props.scenarios[index].name}
+                </button>
+              </animated.span>
             </a>
           );
         })}
-        );
       </div>
     </div>
   );
