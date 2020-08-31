4f474df9392b407607dc73037475f9fc26255f1f
moemedi.mooms <moemedi.mooms@gmail.com>
2020-08-31 19:11:19 +0200

    renamed app & updated site title,description and navbar-brand

--- src/components/presentational/Header/index.js
+++ src/components/presentational/Header/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
-      <span className="navbar-brand mx-auto">when KGOSI reacts</span>
+      <span className="navbar-brand mx-auto">the KGOSI improv show</span>
     </nav>
   );
 };
