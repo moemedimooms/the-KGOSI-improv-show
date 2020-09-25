db2efa821c3e731d6da6f4719d479a4e71bfeb8a
moemedi.mooms <moemedi.mooms@gmail.com>
2020-09-25 12:10:43 +0200

    replaced react-spring animations with their framer-motion equivalents

--- src/components/presentational/DefaultContent/index.js
+++ src/components/presentational/DefaultContent/index.js
@@ -1,52 +1,43 @@
 import React from "react";
-import { useSpring, animated } from "react-spring";
+import { motion } from "framer-motion";
+
+const btnStyles = {
+  paddingRight: "-120px",
+};
 
 const DefaultContent = (props) => {
-  const txtSpring = useSpring({
-    from: {
-      opacity: 0.2,
-    },
-    to: {
-      opacity: 1,
-    },
-    config: {
-      mass: 4,
-    },
-  });
-  const btnSpring = useSpring({
-    from: {
-      opacity: 0,
-      paddingRight: "-10px",
-    },
-    to: {
-      opacity: 1,
-      paddingRight: "0px",
-    },
-    config: {
-      mass: 12,
-    },
-  });
-
   return (
-    <div className="jumbotron">
+    <motion.div
+      className="jumbotron"
+      animate={{ scale: [0.99, 1] }}
+      transition={{ duration: 0.75, easeIn: true }}
+    >
       <p className="h4 font-weight-lighter">
-        <animated.span style={{ ...txtSpring }}>
+        <motion.span
+          animate={{ opacity: [0.2, 1] }}
+          transition={{ duration: 1.2, easeIn: true }}
+        >
           I have a legendary nephew named Kgosi - he can re-enact scenarions
           like an improv pro
-        </animated.span>
+        </motion.span>
       </p>
       <div className="text-right">
-        <animated.span style={{ ...btnSpring }}>
-          <button
-            type="button"
-            class="btn-lg btn-light mt-3"
-            onClick={props.handleViewScenarios}
-          >
-            view scenarios
-          </button>
-        </animated.span>
+        <motion.button
+          type="button"
+          className="btn-lg btn-light mt-3"
+          styles={btnStyles}
+          onClick={props.handleViewScenarios}
+          animate={{
+            opacity: [0.1, 1],
+            paddingRight: "10px",
+            fontSize: ["120%", "118%"],
+          }}
+          transition={{ duration: 1.5, delay: 1 }}
+        >
+          view scenarios
+        </motion.button>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
