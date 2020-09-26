d8122d3a7a327c695fd81948b6614cf49590e35e
moemedi.mooms <moemedi.mooms@gmail.com>
2020-09-26 06:19:36 +0200

    refactored animation code in DefalutContent_component

--- src/components/presentational/DefaultContent/index.js
+++ src/components/presentational/DefaultContent/index.js
@@ -1,40 +1,52 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+// for jumbotron animation
+const jumbotronAnimate = {
+  scale: [0.98, 1.01, 1],
+  borderTopRightRadius: ["55%", "2%"],
+};
+const jumbotronTransition = { duration: 0.75, easeIn: true };
+
+// for p tag animation
+const pAnimate = { opacity: [0.2, 1] };
+const pTransition = { duration: 1.2, easeIn: true };
+
+// for button animation
 const btnStyles = {
   paddingRight: "-120px",
 };
+const btnAnimate = {
+  opacity: [0.1, 1],
+  paddingRight: "10px",
+  fontSize: ["120%", "118%"],
+};
+const btnTransition = { duration: 1.5, delay: 1 };
 
 const DefaultContent = (props) => {
   return (
     <motion.div
       className="jumbotron"
-      animate={{ scale: [0.99, 1] }}
-      transition={{ duration: 0.75, easeIn: true }}
+      animate={jumbotronAnimate}
+      transition={jumbotronTransition}
     >
-      <p className="h4 font-weight-lighter">
-        <motion.span
-          animate={{ opacity: [0.2, 1] }}
-          transition={{ duration: 1.2, easeIn: true }}
-        >
-          I have a legendary nephew named Kgosi - he can re-enact scenarions
-          like an improv pro
-        </motion.span>
-      </p>
+      <motion.p
+        className="h4 font-weight-lighter"
+        animate={pAnimate}
+        transition={pTransition}
+      >
+        I have a nephew named Kgosi, he can re-enact scenes like an improv pro
+      </motion.p>
       <div className="text-right">
         <motion.button
           type="button"
           className="btn-lg btn-light mt-3"
           styles={btnStyles}
           onClick={props.handleViewScenarios}
-          animate={{
-            opacity: [0.1, 1],
-            paddingRight: "10px",
-            fontSize: ["120%", "118%"],
-          }}
-          transition={{ duration: 1.5, delay: 1 }}
+          animate={btnAnimate}
+          transition={btnTransition}
         >
-          view scenarios
+          view scenes
         </motion.button>
       </div>
     </motion.div>
